aa17397a1ec4c84789dac0158b30250aa2201e1d
Jordan Guedj <guedj.jordan@gmail.com>
2025-09-16 15:43:23 +0200

    update IMAGE_VERSION

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -5,7 +5,7 @@ IFS=$'\n\t'
 DOCKER_DEFAULT_PLATFORM="linux/amd64"
 IMAGE_NAMESPACE="registry.gitlab.com/etalab/data.gouv.fr/infra"
 IMAGE_NAME="docker-ansible-git-crypt"
-IMAGE_VERSION="python3.12.7-ansible10.5.0-docker27.3.1-$(git rev-parse --short HEAD)"
+IMAGE_VERSION="python3.12.11-ansible10.5.0-docker28.4.0-$(git rev-parse --short HEAD)"
 
 docker build -t $IMAGE_NAMESPACE/$IMAGE_NAME:$IMAGE_VERSION .
 docker push $IMAGE_NAMESPACE/$IMAGE_NAME:$IMAGE_VERSION
